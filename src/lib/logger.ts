0a2211f3cf85b226106363ad34a75998524871ea
ligarius <ligarius@gmail.com>
2025-10-13 00:04:41 -0300

    Adjust logger for Next runtime

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -4,15 +4,30 @@ import env from '@/config/env';
 const isProduction = env.NODE_ENV === 'production';
 const isTest = env.NODE_ENV === 'test';
 
+const isNextRuntime =
+  typeof process !== 'undefined' && typeof process.env !== 'undefined'
+    ? Boolean(process.env.NEXT_RUNTIME)
+    : false;
+
+const disablePrettyTransport =
+  typeof process !== 'undefined' && typeof process.env !== 'undefined'
+    ? process.env.AMS_DISABLE_PRETTY_LOGGER === 'true'
+    : false;
+
+const shouldUsePrettyTransport =
+  !isProduction && !isTest && !isNextRuntime && !disablePrettyTransport;
+
 const logger = pino({
   level: isProduction ? 'info' : isTest ? 'silent' : 'debug',
-  transport: isProduction || isTest ? undefined : {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:standard',
-    },
-  },
+  transport: shouldUsePrettyTransport
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard',
+        },
+      }
+    : undefined,
 });
 
 export default logger;
